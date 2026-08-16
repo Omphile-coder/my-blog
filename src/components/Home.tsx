e56fe43d89a526e279e8d0fc1b4d3a6e94bddfad
Omphile Lucas <lucasomphile23@gmail.com>
2026-08-16 23:51:52 +0200

    Added a json server

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BlogList } from "./BlogList";
 
 export const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    { title: "web dev tips", body: "lorem ipsum...", author: "mario", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState<any[]>([]);
 
   const handleDelete = (id: number) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    const newBlogs = blogs.filter((blog) => blog !== null && blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  });
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title={"All Blogs"}
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
