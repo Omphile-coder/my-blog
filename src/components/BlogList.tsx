a0cc31da6fa132afe7ef0af1ed72b8eb51a720a4
Omphile Lucas <lucasomphile23@gmail.com>
2026-08-16 22:55:34 +0200

    Made a BlogList component that accepts props and added css styling too

---
+++ src/components/BlogList.tsx
@@ -0,0 +1,13 @@
+export const BlogList = ({ blogs, title }: any) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog: any) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
