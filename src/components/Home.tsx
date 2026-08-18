8df6ccac126df6d45a4b136d8a4ca348c7ef98ee
Omphile Lucas <lucasomphile23@gmail.com>
2026-08-18 06:16:37 +0200

    Added error/Fetch handling to the blog

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,16 +4,25 @@ import { BlogList } from "./BlogList";
 export const Home = () => {
   const [blogs, setBlogs] = useState<any[]>([]);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then((data) => {
           console.log(data);
           setBlogs(data);
+          setIsPending(false);
+          setError("");
+        })
+        .catch((e) => {
+          setError(e.message);
           setIsPending(false);
         });
     }, 1000);
@@ -21,6 +30,7 @@ export const Home = () => {
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>loading ... </div>}
       {blogs && <BlogList blogs={blogs} title={"All Blogs"} />}
     </div>
